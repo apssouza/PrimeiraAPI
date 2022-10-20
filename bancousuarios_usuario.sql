b26c3e789587da94a67fe678b158ff433e628105
Ana Paula <78543051+apssouza@users.noreply.github.com>
2022-10-20 11:17:08 -0300

    Add files via upload

--- bancousuarios_usuario.sql
+++ bancousuarios_usuario.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bancousuarios` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bancousuarios`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bancousuarios
@@ -31,7 +33,7 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `usuario` (`usuario`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +42,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Neymar','neymar10','neymar@gmail.com','1234'),(3,'Ingred','ingred21','ingred22quinha@bol.com','1234');
+INSERT INTO `usuario` VALUES (1,'Neymar','neymar10','neymar@gmail.com','1234'),(3,'Ingred','ingred21','ingred22quinha@bol.com','1234'),(4,'Messi','messi22','messi@gmail.com','1234');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-19  8:06:32
+-- Dump completed on 2022-10-20 11:15:56
